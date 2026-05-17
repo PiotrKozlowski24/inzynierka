0c13ee708a8695b8fa15362ba7664af93de97d19
Piotr Kozłowski <01186976@pw.edu.pl>
2026-05-17 19:36:47 +0200

    feat: Add plots of the real system

--- real_data.m
+++ real_data.m
@@ -125,3 +125,31 @@ legend( ...
 
 title("Układ wtrysku i chłodzenia")
 xlabel("Time")
+
+%% Dynamika całego układu
+figure;
+
+subplot(4,1,1)
+plot(time, pozycja_zaworu_wtrysku_1_strL, 'Color', [0 0.4470 0.7410])
+grid on
+title("Pozycja zaworu")
+
+subplot(4,1,2)
+plot(time, przeplyw_do_schl_1, 'Color', [0.8500 0.3250 0.0980])
+grid on
+title("Przepływ")
+
+subplot(4,1,3)
+plot(time, pomiar_temp_za_wtryskiem_1_strL, 'Color', [0.9290 0.6940 0.1250])
+grid on
+title("Temp za wtryskiem")
+
+subplot(4,1,4)
+plot(time, pomiar_temp_za_przeg_1_strL, 'Color', [0.4940 0.1840 0.5560])
+grid on
+title("Temp za przegrzewaczem")
+
+xlabel("Time")
+
+ax = findobj(gcf,'Type','axes');
+linkaxes(ax,'x')
